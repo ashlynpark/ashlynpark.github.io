a35a0d2c6645b00b92d6d576f326be6557ab7636
ashlynpark <ashlynpark6@gmail.com>
2023-06-19 23:58:36 -0700

    meowwwwww (did all of web home page except flowers)

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,17 +1,54 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import React from 'react';
 
-export default function Home() {
+const Home = ()=> {
   return (
-    <main className={styles.main} style={{ backgroundImage: `url("/bgimagedark.png")` }}>
+    <main className={styles.main} style={{ backgroundImage: `url("/bgimgsparkles.png")`, backgroundSize: 'cover', width: '100%', backgroundPosition: 'center', backgroundRepeat: 'no-repeat'}}>
+      <img className={styles.northstar} src={"/star.png"} alt="Star graphic"/>
       <div className={styles.headercontainer}>
         <h1 className={styles.headerletter}>E</h1>
-        <h1 className={styles.headertitle}>ll's info dot com</h1>
+        <h1 className={styles.headertitle}>
+          ll's info dot com
+          <span className={styles.butterflying}>
+           <img className={styles.butterfly} src={"butterfly.png"} alt="Butterfly graphic"/>
+          </span>
+        </h1>
       </div>
       <div className={styles.frontpagemenu}>
-        
-
+        <MenuItem id="about" image="about.png" name="about"/>
+        <MenuItem id="projects" image="projects.png" name="projects"/>
+        <MenuItem id="contact" image="contact.png" name="contact"/>
+      </div>
+      <div className={styles.subheadingcontainer}>
+        <h2 className={styles.subheading}>developer + designer</h2>
+        <h3 className={styles.subheading2}>technology for a brighter tomorrow</h3>
       </div>
     </main>
   )
+};
+
+interface MenuItemProps {
+  id: string;
+  image: string;
+  name: string;
 }
+
+const MenuItem: React.FC<MenuItemProps> = ({ id, image, name })=> {
+  
+  return(
+    <div id={id} className={styles.unhhhh} style={{marginLeft: '4%', marginRight: '4%', alignContent: 'center', textAlign: 'center'}}>
+      <div className={styles.outerglow}>
+        <div className={styles.itemcontainer}>
+          <img src={image} alt={name} className={styles.circularitem}/>
+        </div>        
+      </div>
+
+      <p className={styles.menutitle}>{name}</p>
+    </div>
+  
+  )
+}
+
+
+export default Home;
