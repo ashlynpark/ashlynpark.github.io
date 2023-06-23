5d9513a9a516dbf2f8e00b980abd36321bcda7f5
ashlynpark <ashlynpark6@gmail.com>
2023-06-23 12:54:09 -0700

    Revert "cdhanged main tag to div"
    This reverts commit b9f15850000980c022d7305655f913c0a17fe968.

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -4,30 +4,27 @@ import React from 'react';
 
 const Home = ()=> {
   return (
-    <main>
-      <div className={styles.main} style={{ backgroundImage: `url("/bgimgsparkles.png")`, backgroundSize: 'cover', width: '100%', backgroundPosition: 'center', backgroundRepeat: 'no-repeat'}}>
-        <img className={styles.northstar} src={"/star.png"} alt="Star graphic"/>
-        <div className={styles.headercontainer}>
-          <h1 className={styles.headerletter}>E</h1>
-          <h1 className={styles.headertitle}>
-            ll's info dot com
-            <span className={styles.butterflying}>
-            <img className={styles.butterfly} src={"butterfly.png"} alt="Butterfly graphic"/>
-            </span>
-          </h1>
-        </div>
-        <div className={styles.frontpagemenu}>
-          <MenuItem id="about" image="about.png" name="about"/>
-          <MenuItem id="projects" image="projects.png" name="projects"/>
-          <MenuItem id="contact" image="contact.png" name="contact"/>
-        </div>
-        <div className={styles.subheadingcontainer}>
-          <h2 className={styles.subheading}>developer + designer</h2>
-          <h3 className={styles.subheading2}>technology for a brighter tomorrow</h3>
-        </div>
-      </div>      
+    <main className={styles.main} style={{ backgroundImage: `url("/bgimgsparkles.png")`, backgroundSize: 'cover', width: '100%', backgroundPosition: 'center', backgroundRepeat: 'no-repeat'}}>
+      <img className={styles.northstar} src={"/star.png"} alt="Star graphic"/>
+      <div className={styles.headercontainer}>
+        <h1 className={styles.headerletter}>E</h1>
+        <h1 className={styles.headertitle}>
+          ll's info dot com
+          <span className={styles.butterflying}>
+           <img className={styles.butterfly} src={"butterfly.png"} alt="Butterfly graphic"/>
+          </span>
+        </h1>
+      </div>
+      <div className={styles.frontpagemenu}>
+        <MenuItem id="about" image="about.png" name="about"/>
+        <MenuItem id="projects" image="projects.png" name="projects"/>
+        <MenuItem id="contact" image="contact.png" name="contact"/>
+      </div>
+      <div className={styles.subheadingcontainer}>
+        <h2 className={styles.subheading}>developer + designer</h2>
+        <h3 className={styles.subheading2}>technology for a brighter tomorrow</h3>
+      </div>
     </main>
-
   )
 };
 
